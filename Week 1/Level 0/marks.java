46e3d198986b6325c025d13e9b8cc4f2b340dfe0
aviksaxena <saxenaavik1@gmail.com>
2025-02-18 13:20:25 +0530

    Add files via upload

---
+++ Week 1/Level 0/marks.java
@@ -0,0 +1,9 @@
+class marks{
+public static void main(String[] args){
+String name=
+"Sam";
+int maths=94,chemistry=96,physics=95;
+double average=(maths+chemistry+physics)/(3);
+System.out.print(name+"'s average marks in PCM is "+average);
+}
+}
