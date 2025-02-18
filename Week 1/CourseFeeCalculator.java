9ec494d89b020e13ba505ad038c8fd1622af410e
aviksaxena <saxenaavik1@gmail.com>
2025-02-18 13:15:35 +0530

    Delete Week 1 directory

--- Week 1/CourseFeeCalculator.java
+++
@@ -1,9 +0,0 @@
-public class CourseFeeCalculator {
-    public static void main(String[] args) {
-        double courseFee = 125000;
-        double discount = courseFee * 0.10;
-        double discountedPrice = courseFee - discount;
-        System.out.println("Discounted amount: INR " + discount);
-        System.out.println("Total price after discount: INR " + discountedPrice);
-    }
-}
--- Week 1/marks.java
+++
@@ -1,9 +0,0 @@
-class marks{
-public static void main(String[] args){
-String name=
-"Sam";
-int maths=94,chemistry=96,physics=95;
-double average=(maths+chemistry+physics)/(3);
-System.out.print(name+"'s average marks in PCM is "+average);
-}
-}
